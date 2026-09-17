8dc4a2cd55c5d17f7b05489c3f5e3d5f3225c53e
Gustavo <gustavoabreu2228@gmail.com>
2026-09-17 19:58:46 -0300

    upload atividade 17-09

---
+++ aula 17-09/restaurante.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define taxa 0.10
+
+int main() {
+    float totalConsumo, taxaServico, totalConta, valorIndividual;
+    int qtdPessoas;
+    printf("----Fechamento Conta Restaurante------\n");
+    printf("Digite o total do consumo em reais: ");
+    scanf("%f", &totalConsumo);
+    printf("Digite a quantidade de pessoas: ");
+    scanf("%d", &qtdPessoas);
+
+    taxaServico = totalConsumo * taxa;
+    totalConta = totalConsumo + taxaServico;
+    valorIndividual = totalConta / qtdPessoas;
+
+
+    printf("\n----Resumo da Conta------\n");
+    printf("Total do consumo: R$ %.2f\n", totalConsumo);
+    printf("Taxa de serviço: R$ %.2f\n", taxaServico);
+    printf("------------------------------\n");
+    printf("Total da conta: R$ %.2f\n", totalConta);
+    printf("Valor individual: R$ %.2f\n", valorIndividual);
+
+    return 0;
+}
